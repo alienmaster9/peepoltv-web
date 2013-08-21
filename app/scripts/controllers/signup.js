8e1c4686f110f9b7435cf29a0818fa6fb75f8d57
Juan Ignacio Donoso <jidonoso@gmail.com>
2013-08-21 17:24:37 -0400

    Add username to create account modal

--- app/scripts/controllers/signup.js
+++ app/scripts/controllers/signup.js
@@ -15,10 +15,14 @@ angular.module('peepoltvApp')
         //authService.cookies.setToken(e.data['auth_token']);
       },
       function(e){
-        if(angular.isObject(e.data) && e.data.info.email.length){
-          if(e.data.info.email[0] === 'has already been taken'){
+        if(angular.isObject(e.data)){
+          if(e.data.info.email && e.data.info.email[0] === 'has already been taken'){
             // The email is already taken
             $scope.alert = 'email';
+          }
+          else if(e.data.info.username && e.data.info.username[0] === 'has already been taken'){
+            // The email is already taken
+            $scope.alert = 'username';
           }
           else{
             $scope.alert = 'other';
